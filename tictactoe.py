a398dbc5c2d33478a691c5a968e75f2c58758e43
Bijin Nhuchhe Pradhan <bijinpradhan@gmail.com>
2019-10-04 12:27:58 +0530

    Cleaned up tictactoe a bit

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,11 @@
 turn = 'X'
 win = False
+spaces = 9
 
 
 def draw(board):
     global turn
-    for i in range(0, 3):
+    for i in range(2, -1, -1):
         print(' ' + board[i*3+1] + '|' +
               board[i*3+2] + '|' + board[i*3+3])
 
@@ -15,12 +16,17 @@ def takeinput(board):
     print(turn + "'s turn:")
 
     while pos == -1:
-        print("Pick position 1-9:")
-        pos = int(input())
-        if(pos < 1 or pos > 9):
-            pos = -1
-        if board[pos] != ' ':
-            pos = -1
+        try:
+            print("Pick position 1-9:")
+            pos = int(input())
+            if(pos < 1 or pos > 9):
+                pos = -1
+            if board[pos] != ' ':
+                pos = -1
+            if pos != -1:
+                spaces -= 1
+        except:
+            print("enter a valid position")
 
     board[pos] = turn
     if turn == 'X':
@@ -35,24 +41,28 @@ def checkwin(board):
         if board[i*3+1] == board[i*3+2] and board[i*3+1] == board[i*3+3]:
             if not(board[i*3+1] == ' '):
                 win = True
-                break
+                return board[i*3+1]
         elif board[1+i] == board[4+i] and board[1+i] == board[7+i]:
             if not(board[i+1] == ' '):
                 win = True
-                break
+                return board[1+i]
 
     if (board[1] == board[5] and board[5] == board[9]):
         if board[1] != ' ':
             win = True
+            return board[1]
 
     if (board[3] == board[5] and board[5] == board[7]):
         if board[3] != ' ':
             win = True
+            return board[3]
+
+    return 'na'
 
 
 board = [' ']*10
 
-while not win:
+while not win and spaces:
     draw(board)
 
     takeinput(board)
@@ -60,3 +70,8 @@ while not win:
     checkwin(board)
 
 draw(board)
+
+if not win and not spaces:
+    print("draw")
+elif win:
+    print('{0} wins'.format(checkwin(board)))
